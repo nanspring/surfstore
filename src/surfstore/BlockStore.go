a6bcff548123e2ffdef621ce2bc38252b7100ddb
EC2 Default User <ec2-user@ip-172-31-2-35.us-west-2.compute.internal>
2020-11-12 14:58:36 +0000

    fill block get put method

--- src/surfstore/BlockStore.go
+++ src/surfstore/BlockStore.go
@@ -1,19 +1,36 @@
 package surfstore
 
+import(
+	"errors"
+	"crypto/sha256"
+	"encoding/hex"
+)
 type BlockStore struct {
 	BlockMap map[string]Block
 }
 
 func (bs *BlockStore) GetBlock(blockHash string, blockData *Block) error {
-	panic("todo")
+	//panic("todo")
+	if val, ok := bs.BlockMap[blockHash] ; ok {
+		blockData = bs.BlockMap[blockHash]
+		return nil
+	}else{
+		return errors.New("Block does not exist")
+	}
 }
 
 func (bs *BlockStore) PutBlock(block Block, succ *bool) error {
-	panic("todo")
+	//panic("todo")
+	h := sha256.New()
+	h.Write(block.BlockData)
+	hash_code := hex.EncodeToString(h.Sum(nil))
+	bs.BlockMap[hash_code] = block
+	return nil
 }
 
 func (bs *BlockStore) HasBlocks(blockHashesIn []string, blockHashesOut *[]string) error {
-	panic("todo")
+	//panic("todo")
+
 }
 
 // This line guarantees all method for BlockStore are implemented
